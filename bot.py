6bc19d97dccfff92c1d4459c78ff2e5ebef14ff9
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2023-07-08 08:50:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import logging, asyncio
+import logging
+import asyncio
+import os
 
 from os import environ
 from pyrogram import Client, filters
@@ -6,8 +8,6 @@ from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 
 logging.basicConfig(level=logging.ERROR)
-
-import os
 
 channels_str = os.environ.get("CHANNELS")
 if channels_str is not None:
@@ -17,33 +17,38 @@ else:
     CHANNELS = []
 
 AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
-User     = Client(name = "AcceptUser", session_string = environ.get("SESSION"))
+User = Client(
+    "AcceptUser",
+    api_id=int(environ.get("API_ID")),
+    api_hash=environ.get("API_HASH"),
+    session_string=environ.get("SESSION")
+)
 
 
 @User.on_message(filters.command(["run", "approve", "start"], [".", "/"]) & AuthChat)                     
 async def approve(client: User, message: Message):
     Id = message.chat.id
     await message.delete(True)
- 
+
     try:
-       while True: # create loop is better techniq 🙃
-           try:
-               await client.approve_all_chat_join_requests(Id)         
-           except FloodWait as t:
-               asyncio.sleep(t.value)
-               await client.approve_all_chat_join_requests(Id) 
-           except Exception as e:
-               logging.error(str(e))
-    except FloodWait as s:
-        asyncio.sleep(s.value)
         while True:
-           try:
-               await client.approve_all_chat_join_requests(Id)         
-           except FloodWait as t:
-               asyncio.sleep(t.value)
-               await client.approve_all_chat_join_requests(Id) 
-           except Exception as e:
-               logging.error(str(e))
+            try:
+                await client.approve_all_chat_join_requests(Id)
+            except FloodWait as t:
+                await asyncio.sleep(t.x)  # Sleep for the specified flood wait time
+                await client.approve_all_chat_join_requests(Id)
+            except Exception as e:
+                logging.error(str(e))
+    except FloodWait as s:
+        await asyncio.sleep(s.x)  # Sleep for the specified flood wait time
+        while True:
+            try:
+                await client.approve_all_chat_join_requests(Id)
+            except FloodWait as t:
+                await asyncio.sleep(t.x)  # Sleep for the specified flood wait time
+                await client.approve_all_chat_join_requests(Id)
+            except Exception as e:
+                logging.error(str(e))
 
     msg = await client.send_message(Id, "**Task Completed** ✓ **Approved Pending All Join Request**")
     await asyncio.sleep(3)
@@ -52,10 +57,4 @@ async def approve(client: User, message: Message):
 
 logging.info("Bot Started....")
 User.run()
-
-
-
-
-
-
 
